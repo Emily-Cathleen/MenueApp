3ce4eb31e53f9130b79821b8e0746e7f96d2bacb
Emily Corcoran <cathleencorcoran@gmail.com>
2023-01-24 10:53:11 -0500

    initial commit

---
+++ screens/MealsOverviewScreen.js
@@ -0,0 +1,36 @@
+import { View, FlatList, StyleSheet } from "react-native";
+import MealItem from "../components/MealItem";
+
+import { MEALS } from "../data/dummy-data";
+
+function MealsOverviewScreen({ route }) {
+  const catId = route.params.categoryId;
+
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0;
+  });
+
+  function renderMealItem(itemData) {
+    return <MealItem title={itemData.item.title}/>
+  }
+
+
+  return (
+    <View styles={styles.container}>
+      <FlatList 
+      data={displayedMeals} 
+      keyExtractorenr={(item ) => item.id}
+      renderItem={renderMealItem}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+});
+
+export default MealsOverviewScreen;
